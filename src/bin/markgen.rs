d72843ee946199dfb8738c2f4690211f898ac8cb
Paul Woolcock <paul@woolcock.us>
2017-07-04 07:38:51 -0400

    update should_panic attribute

--- src/bin/markgen.rs
+++ src/bin/markgen.rs
@@ -97,19 +97,19 @@ mod test {
     }
 
     #[test]
-    #[should_panic(message = "No files were fed into the chain.")]
+    #[should_panic(expected = "No files were fed into the chain.")]
     fn gen_invalid_no_files() {
         markov_gen(vec!["prog".to_owned(), "-n".to_owned(), "3".to_owned()]);
     }
 
     #[test]
-    #[should_panic(message = "Expected positive integer argument to -n, found 0.")]
+    #[should_panic(expected = "Expected positive integer argument to -n, found 0.")]
     fn gen_invalid_n_arg_zero() {
         markov_gen(vec!["prog".to_owned(), "test".to_owned(), "-n".to_owned(), "0".to_owned()]);
     }
 
     #[test]
-    #[should_panic(message = "Expected positive integer argument to -n, found test.")]
+    #[should_panic(expected = "Expected positive integer argument to -n, found test.")]
     fn gen_invalid_n_arg_string() {
         markov_gen(vec!["prog".to_owned(), "test".to_owned(), "-n".to_owned(), "test".to_owned()]);
     }
