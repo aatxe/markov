fa12d3634235a04420bf082c5bcc3de30546f34d
Nystik-gh <1017253+Nystik-gh@users.noreply.github.com>
2022-09-06 00:00:16 +0200

    remove unused import

--- src/lib.rs
+++ src/lib.rs
@@ -57,7 +57,7 @@ use std::path::Path;
 use itertools::Itertools;
 #[cfg(feature = "graph")]
 use petgraph::graph::Graph;
-use rand::{thread_rng, Rng, SeedableRng};
+use rand::{thread_rng, Rng};
 #[cfg(feature = "yaml")]
 use serde::de::DeserializeOwned;
 #[cfg(feature = "yaml")]
